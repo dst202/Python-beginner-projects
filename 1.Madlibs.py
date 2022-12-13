55cd6fe02db16bd56f37239e505b8397ba3363d6
devi surya teja ch <suryatejach202@gmail.com>
2022-12-13 10:28:54 +0100

    Completed 1.Madlibs.py

---
+++ 1.Madlibs.py
@@ -0,0 +1,18 @@
+# ways  to create a madlib
+youtuber="Linus tech tips"
+
+#methods
+#print("subscribe to "+youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adv1=input("adverb1:")
+noun=input("noun:")
+adv2=input("adverb2:")
+youtuber=input("Youtuber:")
+
+madlib=f"Coding is so much {adv1}.\
+it sometimes make me fell like a {noun}.\
+Just sit tight and stay {adv2}.\
+Subscribe to {youtuber}."
+print(madlib)
